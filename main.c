8aa483cee60cb0ef70aa49dad4767174aa5d3dc2
Pedro Silva <pedrohenriquefesi@gmail.com>
2024-03-18 18:44:57 -0300

    hfiehiofheiwhhifehephfe

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "graph.h"
+
+int menu() {
+  printf("Digite uma opção:\n");
+  printf("1 - Verificar se uma aresta existe\n");
+  printf("2 - Arestas adjacentes a determinado vértice\n");
+  printf("3 - Verificar se uma aresta existe\n");
+  printf("4 - Verificar se uma aresta existe\n");
+  printf("5 - Verificar se uma aresta existe\n");
+  printf("6 - Verificar se uma aresta existe\n");
+  printf("7 - Verificar se uma aresta existe\n");
+  printf("8 - Verificar se uma aresta existe\n");
+  printf("9 - Verificar se uma aresta existe\n");
+  printf("10 - Verificar se uma aresta existe\n");
+}
+
+int main() {
+  GRAFO *grafo;
+  int N;
+  scanf("%d", &N);
+  grafo = MyGraph(N);
+
+  
+
+  return 0;
+}
