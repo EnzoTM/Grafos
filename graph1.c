61223d7dda9055f509d0184e5db025ceb96491a1
Pedro Silva <pedrohenriquefesi@gmail.com>
2024-03-18 17:48:11 -0300

    fhiohfeiwhfehoefihofehoife

--- graph1.c
+++ graph1.c
@@ -54,10 +54,3 @@ int exist_edge(GRAFO* grafo, int v1, int v2){
 
 }
 int ** get_adj_vertex(GRAFO* grafo, int posicao);
-int remove_edge(GRAFO* grafo, int v1, int v2);
-void print_info(GRAFO * grafo);
-int number_of_vertexs(GRAFO * grafo);
-int remove_graph(GRAFO* grafo);
-int add_edge(GRAFO* grafo, int v1, int v2);
-int remove_less_weight(GRAFO* grafo);
-int * adjacency_matrix(GRAFO* grafo);
